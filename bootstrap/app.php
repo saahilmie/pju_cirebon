885d35604439a79945ebc75b6e4117324152b065
saahilmie <khonsaahilmiee@gmail.com>
2026-02-04 11:36:50 +0700

    Fix HTTPS/CSRF: trust Railway proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust all proxies (Railway runs behind a proxy)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
